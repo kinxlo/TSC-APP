9ca5d6041f78affc47a5c8eba821c62819f8a64c
Kingsley Solomon <kinxly@gmail.com>
2023-11-17 19:49:14 +0100

    init: setup linting and redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
     return (
         <>
-            <h1>TSC-APP</h1>
+            <h1 className={`text-danger`}>TSC-APP</h1>
         </>
     );
 }
---
+++ src/pages/external-pages/index.js
@@ -0,0 +1,1 @@
+export { default as FindTalent } from "./FindTalent";
