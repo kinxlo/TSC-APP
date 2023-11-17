e18e5c914b73d54991c146748e509ec9070f704f
Kingsley Solomon <kinxly@gmail.com>
2023-11-17 20:11:37 +0100

    feat: added routes

---
+++ src/layout/DashboardLayout/DashboardLayout.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// eslint-disable-next-line react/prop-types
+const DashboardLayout = ({ children }) => {
+  return <div>{children}</div>
+}
+
+export default DashboardLayout
