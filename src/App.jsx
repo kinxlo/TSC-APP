cf177204ec24162d14eb508d2efb1667c7009573
Kingsley Solomon <kinxly@gmail.com>
2023-11-28 09:20:19 +0100

    refactor: added authentication page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
-import { DashboardHome, Login, Community } from './pages'
+// import { DashboardHome, Login, Community } from './pages'
 import { DashboardLayout } from './layout'
 import './App.css'
 import Home from './pages/external-pages/landing-page/Home/Home'
+import AuthLayout from './layout/AuthLayout'
+import SignIn from './pages/auth-pages/SignIn'
+import SignUp from './pages/auth-pages/SignUp'
+import Community from './pages/external-pages/explore-community/Community'
 
 export function App() {
   return (
     <Routes>
       {/* authentication */}
-      <Route path={`/login`} element={<Login />} />
+      <Route element={<AuthLayout />}>
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Route>
 
       {/* external pages */}
       <Route index element={<Home />} />
@@ -16,7 +23,7 @@ export function App() {
 
       {/* dashboard */}
       <Route path='/dashboard' element={<DashboardLayout />}>
-        <Route path='home' index element={<DashboardHome />} />
+        {/* <Route path='home' index element={<DashboardHome />} /> */}
         <Route path='*' element={<h1>PAGE NOT FOUND</h1>} />
       </Route>
       <Route path='*' element={<h1>PAGE NOT FOUND</h1>} />
